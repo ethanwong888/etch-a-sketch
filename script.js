5971a623926786203d009f60a3517639658f2f05
Ethan Wong <ethanwong@g.ucla.edu>
2023-10-26 14:52:52 -0700

    added drawing capabilities

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const container = document.querySelector(".container")
 
 for (let i = 0; i < 256; i++) {
-  let div = document.createElement("div");
-  div.setAttribute("id", "pixel");
-  container.appendChild(div);
+  let pixelDiv = document.createElement("div");
+  pixelDiv.setAttribute("id", "pixel");
+  pixelDiv.addEventListener('mouseover', draw)
+  pixelDiv.addEventListener('mousedown', draw)
+  container.appendChild(pixelDiv);
+}
+
+
+let mouseClicked = false
+document.body.onmousedown = () => (mouseClicked = true)
+document.body.onmouseup = () => (mouseClicked = false)
+
+function draw(e){
+  if (e.type === 'mouseover' && !mouseClicked) return
+  if (mouseClicked) {
+    e.target.style.backgroundColor = '#333333';
+  }
   
 }
-// let div = document.createElement("div");
-// container.appendChild(div);
